49806483088749d23d4ac19c2803175267f0ba49
Benjamin Durin <benjamin.durin@neurovalue.com>
2022-02-25 15:44:18 +0100

    Fixed filename

--- lib/rb1drv/onedrive_item.rb
+++ lib/rb1drv/onedrive_item.rb
@@ -2,9 +2,9 @@ module Rb1drv
   class OneDriveItem
     attr_reader :id, :name, :eTag, :size, :mtime, :ctime, :muser, :cuser, :parent_path, :remote_id, :remote_drive_id
 
-    def copy(destination)
-      destination_folder = @od.get(destination)
-      result = @od.request("#{api_path}/copy", { parentReference: { driveId: @drive_id, id: destination_folder.id }, name: @name }, :post)
+    def copy(destination_path, filename)
+      destination_folder = @od.get(destination_path)
+      result = @od.request("#{api_path}/copy", { parentReference: { driveId: @drive_id, id: destination_folder.id }, name: filename }, :post)
       puts result.inspect
     end
 
